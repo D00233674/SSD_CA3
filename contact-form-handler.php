2cad6f2b706c82e5c04728c76949690541b0eb72
Kieran <D00233674@student.dkit.ie>
2021-04-14 11:37:23 +0100

    added regex/js validation to the new form fields

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -2,17 +2,33 @@
 $errors = '';
 $myemail = 'd00233674@student.dkit.ie';//<-----Put your DkIT email address here.
 if(empty($_POST['name'])  || 
+   empty($_POST['dob']) || 
    empty($_POST['email']) || 
-   empty($_POST['age']) || 
+   empty($_POST['phone']) || 
    empty($_POST['message']))
 {
     $errors .= "\n Error: all fields are required";
 }
 
 $name = $_POST['name']; 
+$dob = $_POST['dob']; 
 $email_address = $_POST['email']; 
-$age = $_POST['age']; 
+$phone = $_POST['phone']; 
 $message = $_POST['message']; 
+
+if (!preg_match(
+"/^[a-zA-Z ]+$/", 
+$name))
+{
+	$errors .= "\n Error: Invalid name field";
+}
+
+if (!preg_match(
+"/^(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))$/", 
+$dob))
+{
+	$errors .= "\n Error: Invalid date of birth";
+}
 
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
@@ -21,12 +37,21 @@ $email_address))
     $errors .= "\n Error: Invalid email address";
 }
 
+if (!preg_match(
+"/^\+(353|44)(\d){8}$/", 
+$phone))
+{
+	$errors .= "\n Error: Invalid phone number";
+}
+
+
+
 if( empty($errors))
 {
 	$to = $myemail; 
 	$email_subject = "Contact form submission: $name";
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Age: $age \n Message \n $message"; 
+	" Here are the details:\n Name: $name \n Date of Birth: $dob \n Email: $email_address \n Phone Number: $phone \n Message \n $message"; 
 	
 	$headers = "From: $myemail\n"; 
 	$headers .= "Reply-To: $email_address";
@@ -45,9 +70,9 @@ include('includes/header.php');
 echo nl2br($errors);
 ?>
 
-<!-- return to form or homepage??? -->
-
-
+<!-- return to form or homepage??? can just use nav
+<p><a href="contact.php" class="manage-button">Return to Form</a></p>
+-->
 <?php
 include('includes/footer.php');
 ?>
--- contact.php
+++ contact.php
@@ -11,16 +11,20 @@ include('includes/header.php');
 <input type="text" name="name">
 </p>
 <p>
+<label for='dob'>Date of Birth:</label> <br>
+<input type="text" name="dob">
+</p>
+<p>
 <label for='email'>Email Address:</label> <br>
 <input type="text" name="email"> <br>
 </p>
 <p>
-<label for='age'>Age:</label> <br>
-<input type="text" name="age"> <br>
+<label for='phone'>Phone Number:</label> <br>
+<input type="text" name="phone"> <br>
 </p>
 <p>
 <label for='message'>Message:</label> <br>
-<textarea name="message"></textarea>
+<textarea name="message" rows=5 cols=30></textarea>
 </p>
 <input type="submit" value="Submit"><br>
 </form>
@@ -28,8 +32,10 @@ include('includes/header.php');
 <script language="JavaScript">
 var frmvalidator  = new Validator("contactform");
 frmvalidator.addValidation("name","req","Please provide your name"); 
+frmvalidator.addValidation("dob","req","Please provide your date of birth");
 frmvalidator.addValidation("email","req","Please provide your email"); 
 frmvalidator.addValidation("email","email","Please enter a valid email address"); 
+frmvalidator.addValidation("phone","req","Please provide your phone number");
 </script>
 
 
